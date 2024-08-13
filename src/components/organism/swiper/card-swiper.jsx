50073e99a21f0f48cd2ae9d709d514476eba18f1
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-08-13 11:53:42 +0700

    fix

--- src/components/organism/swiper/card-swiper.jsx
+++ src/components/organism/swiper/card-swiper.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Image from 'next/image';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "next/image";
 
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-import dynamic from 'next/dynamic';
-import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from '@heroicons/react/20/solid';
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+import dynamic from "next/dynamic";
+import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from "@heroicons/react/20/solid";
 
 // import slide_image_1 from '/images/img_1.jpg';
 // import slide_image_2 from '/images/img_2.jpg';
@@ -19,18 +17,19 @@ import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from '@heroicons/react/20/s
 // import slide_image_4 from '/images/img_4.jpg';
 // import slide_image_5 from '/images/img_5.jpg';
 
-
-const slide_image_1 = dynamic(()=>import("../../../../public/images/img_1.jpg"))
+const slide_image_1 = dynamic(() => import("../../../../public/images/img_1.jpg"));
 
 function App() {
   return (
     <div className="container">
-      <h1 className="heading">HVC Anda</h1>
+      <h1 className="heading" style={{ fontStretch: "ultra-expanded" }}>
+        HVC Anda
+      </h1>
       <Swiper
-        effect={'coverflow'}
+        effect={"coverflow"}
         grabCursor={true}
         loop={true}
-        slidesPerView={'3'}
+        slidesPerView={"3"}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
@@ -39,43 +38,105 @@ function App() {
         }}
         // pagination={{ el: '.swiper-pagination', clickable: true }}
         navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
           clickable: true,
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
         className="swiper_container"
       >
         <SwiperSlide>
-            <div className='shadow-lg bg-blue-400'></div>
+          <div style={{ width: "100%", position: "relative" }} className=" min-w-[355px] max-w-[500px]">
+            <Image
+              alt="slide_1"
+              src="/images/master_card/b.png"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
+            <div className="absolute right-6 xl:right-9 bottom-6 xl:bottom-9 text-white text-right xl:text-xl">
+              <p className="leading-5 xl:leading-8 font-sans font-bold">Wahyu Hidayat</p>
+              <p className="leading-5 xl:leading-6 font-sans xl:text-2xl font-bold">
+                0101 &nbsp;0101 &nbsp;0101 &nbsp;0101
+              </p>
+              <p className="leading-5 xl:leading-6 font-sans font-bold uppercase">jk12341sa</p>
+              <p className="leading-5 xl:leading-6 font-sans">Exp. 02/17</p>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-            {/* <Image src={"/images/img_1.jpg"} layout='fill' objectFit='cover' alt='slide_1'/> */}
-            <div className='shadow-lg bg-red-400'></div>
+          <div style={{ width: "100%", position: "relative" }} className=" min-w-[355px] max-w-[500px]">
+            <Image
+              alt="slide_1"
+              src="/images/master_card/b.png"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
+            <div className="absolute right-6 xl:right-9 bottom-6 xl:bottom-9 text-white text-right xl:text-xl">
+              <p className="leading-5 xl:leading-8 font-sans font-bold">Wahyu Hidayat</p>
+              <p className="leading-5 xl:leading-6 font-sans xl:text-2xl font-bold">
+                0101 &nbsp;0101 &nbsp;0101 &nbsp;0101
+              </p>
+              <p className="leading-5 xl:leading-6 font-sans font-bold uppercase">jk12341sa</p>
+              <p className="leading-5 xl:leading-6 font-sans">Exp. 02/17</p>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-            {/* <Image src={"/images/img_1.jpg"} layout='fill' objectFit='cover' alt='slide_1'/> */}
-            <div className='shadow-lg bg-yellow-400'></div>
+          <div style={{ width: "100%", position: "relative" }} className=" min-w-[355px] max-w-[500px]">
+            <Image
+              alt="slide_1"
+              src="/images/master_card/b.png"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
+            <div className="absolute right-6 xl:right-9 bottom-6 xl:bottom-9 text-white text-right xl:text-xl">
+              <p className="leading-5 xl:leading-8 font-sans font-bold">Wahyu Hidayat</p>
+              <p className="leading-5 xl:leading-6 font-sans xl:text-2xl font-bold">
+                0101 &nbsp;0101 &nbsp;0101 &nbsp;0101
+              </p>
+              <p className="leading-5 xl:leading-6 font-sans font-bold uppercase">jk12341sa</p>
+              <p className="leading-5 xl:leading-6 font-sans">Exp. 02/17</p>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-            {/* <Image src={"/images/img_1.jpg"} layout='fill' objectFit='cover' alt='slide_1'/> */}
-            <div className='shadow-lg bg-green-400'></div>
-        </SwiperSlide>
-        <SwiperSlide>
-            {/* <Image src={"/images/img_1.jpg"} layout='fill' objectFit='cover' alt='slide_1'/> */}
-            <div className='shadow-lg bg-slate-400'></div>
+          <div style={{ width: "100%", position: "relative" }} className=" min-w-[355px] max-w-[500px]">
+            <Image
+              alt="slide_1"
+              src="/images/master_card/b.png"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
+            <div className="absolute right-6 xl:right-9 bottom-6 xl:bottom-9 text-white text-right xl:text-xl">
+              <p className="leading-5 xl:leading-8 font-sans font-bold">Wahyu Hidayat</p>
+              <p className="leading-5 xl:leading-6 font-sans xl:text-2xl font-bold">
+                0101 &nbsp;0101 &nbsp;0101 &nbsp;0101
+              </p>
+              <p className="leading-5 xl:leading-6 font-sans font-bold uppercase">jk12341sa</p>
+              <p className="leading-5 xl:leading-6 font-sans">Exp. 02/17</p>
+            </div>
+          </div>
         </SwiperSlide>
 
         <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
-            <ArrowLeftCircleIcon className='h-6 w-6'/>
+            <ArrowLeftCircleIcon className="h-6 w-6" />
           </div>
           <div className="swiper-button-next slider-arrow">
-            <ArrowRightCircleIcon  className='h-6 2-6'/>
+            <ArrowRightCircleIcon className="h-6 2-6" />
           </div>
           {/* <div className="swiper-pagination"></div> */}
         </div>
       </Swiper>
+
     </div>
   );
 }
