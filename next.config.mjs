51aa7e3dfab44ed64b9d7f81213b9a585ab8849c
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-12-26 10:55:00 +0700

    benerin config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "103.163.139.156", "green-m.xyz"],
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
@@ -31,6 +31,27 @@ const nextConfig = {
         protocol: "http",
         hostname: "localhost",
         port: "3003",
+        pathname: "/uploads/**", // Match any path under /images/
+      },
+      {
+        // Matches any image hosted on localhost:3002
+        protocol: "http",
+        hostname: "103.163.139.156",
+        port: "3003",
+        pathname: "/uploads/**", // Match any path under /images/
+      },
+      {
+        // Matches any image hosted on localhost:3002
+        protocol: "http",
+        hostname: "green-m.xyz",
+        port: "80",
+        pathname: "/uploads/**", // Match any path under /images/
+      },
+      {
+        // Matches any image hosted on localhost:3002
+        protocol: "https",
+        hostname: "green-m.xyz",
+        port: "443",
         pathname: "/uploads/**", // Match any path under /images/
       },
     ],
